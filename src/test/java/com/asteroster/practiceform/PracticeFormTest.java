2e6e89557801c5e4216dfbe81fc66ad0f354b1d6
LutSerg <5QGJAZCqesdHUvk>
2023-03-13 18:26:20 +0300

    added assertions

--- src/test/java/com/asteroster/practiceform/PracticeFormTest.java
+++ src/test/java/com/asteroster/practiceform/PracticeFormTest.java
@@ -1,9 +1,11 @@
 package com.asteroster.practiceform;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -19,8 +21,8 @@ public class PracticeFormTest {
     }
     @Test
     void practiceFormTest () {
-        $("#firstName").setValue("Some name");
-        $("#lastName").setValue("Some surname");
+        $("#firstName").setValue("Фантомас");
+        $("#lastName").setValue("Багровый");
         $("#userEmail").setValue("Some_email@email.com");
         $("[for = gender-radio-1]").click();
         $("#userNumber").setValue("4415639874");
@@ -38,5 +40,18 @@ public class PracticeFormTest {
         $("#city").click();
         $(byText("Lucknow")).click();
         $("#submit").click();
+        $(".modal-header").shouldBe(visible)
+                .shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive")
+                .shouldHave(text("Фантомас Багровый"))
+                .shouldHave(text("Some_email@email.com"))
+                .shouldHave(text("Male"))
+                .shouldHave(text("4415639874"))
+                .shouldHave(text("23 August,2001"))
+                .shouldHave(text("History"))
+                .shouldHave(text("Music"))
+                .shouldHave(text("Uttar Pradesh Lucknow"));
+        $("#closeLargeModal").click();
+
     }
 }
